49579925d58fa717c430475d9980a179a9203f76
wuwzhang <wuwzhang@gmail.com>
2023-01-08 16:28:46 +0800

    feat: 去掉首页模块

--- web/src/pages/CollectPage/CollectPage.js
+++ web/src/pages/CollectPage/CollectPage.js
@@ -2,8 +2,7 @@ import BlogPostsCell from 'src/components/BlogPostsCell/BlogPostsCell.js'
 // import BlogCatalogPostsCell from 'src/components/BlogCatalogPostsCell/BlogCatalogPostsCell.js'
 import BlogLayout from 'src/layouts/BlogLayout/BlogLayout.js'
 
-const CollectPage
- = () => {
+const CollectPage = () => {
   return (
     <BlogLayout>
       <BlogPostsCell catalog="collect" size={10} />
